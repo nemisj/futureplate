19322ec3c3635d10d27b6eddff8488278efc643a
Maks Nemisj <maks@nemisj.com>
2016-04-05 11:22:36 +0200

    separate rendering

--- src/server.js
+++ src/server.js
@@ -1,17 +1,8 @@
 import express from 'express';
 import _ from 'lodash';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
 import favicon from 'serve-favicon';
-import serialize from 'serialize-javascript';
 
-import routes from './routes';
-
-import IndexStore from './stores/index';
-import { getDependencies } from './utils/index';
-import { FluxContext } from './utils/wrappers';
-
+import renderer from './renderer.js';
 
 const DEVELOPMENT = process.env.NODE_ENV === 'development';
 const HOT_MODULE_REPLACEMENT = DEVELOPMENT && process.env.HMR;
@@ -39,46 +30,7 @@ if (!HOT_MODULE_REPLACEMENT) {
   app.use('/static', express.static('build/client'));
 }
 
-
-app.get('/*', function(req, res) {
-  const location = req.url;
-  match({ routes, location }, (error, redirect, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message);
-
-    } else if (redirect) {
-      res.redirect(302, redirect.pathname + redirect.search);
-
-    } else if (renderProps) {
-      const store = new IndexStore();
-      const dependencies = getDependencies(renderProps.routes,
-                                           store,
-                                           renderProps.params);
-      Promise.all(dependencies)
-        .then(() => {
-          const content = renderToString((
-            <FluxContext store={store}>
-              <RouterContext {...renderProps} />
-            </FluxContext>
-          ));
-          const data = serialize(store.serialize());
-          res.render('index', {
-            content,
-            data,
-            development: DEVELOPMENT,
-            base: HOT_MODULE_REPLACEMENT ? 'http://localhost:8080' : '',
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(404).send('Not found');
-        });
-    } else {
-      console.log(location);
-      res.status(404).send('Not found');
-    }
-  });
-});
+app.get('/*', renderer);
 
 const server = app.listen(app.get('port'), function() {
   const host = server.address().address;
